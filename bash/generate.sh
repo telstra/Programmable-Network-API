7eb8156b5218f7b3943c6435e487c76ba413349e
developersteve <steven@developersteve.com>
2018-01-29 17:54:10 +1100

    updated gen bash and config scripts

--- bash/generate.sh
+++ bash/generate.sh
@@ -3,13 +3,20 @@
 
 for file in ../spec_config/*.json
 do	
-
     if [[ -f $file ]]; then
+
     	s=${file##*/}
     	fname=${s%.*}
+
     	mkdir -p ../../test/$fname
 
-		eval swagger-codegen generate -i ../docs/openapi/programmable-network-swagger20.yaml -l $fname -c $file -o ../../test/$fname    	
+		eval swagger-codegen generate -i ../docs/openapi/programmable-network-swagger20.yaml -l $fname -c $file -o ../../test/$fname
+
+		rm ../../test/$fname/git_push.sh
+
+		sed -i '' "/$Swagger Codegen/d" ../../test/$fname/README.md 
+		sed -i '' "/$Build package/d" ../../test/$fname/README.md
+		sed -i '' "/$swagger-codegen/d" ../../test/$fname/README.md
 
     fi
 done
